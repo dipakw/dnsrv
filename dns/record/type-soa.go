fdab4638f00bae4512528e031d49e4e35200c021
Dipak Acharya <dev.dipak21@gmail.com>
2024-08-12 13:06:19 +0545

    Fix SOA

--- dns/record/type-soa.go
+++ dns/record/type-soa.go
@@ -3,20 +3,22 @@ package record
 import "encoding/binary"
 
 func (r *SOA) Encode() []*Answer {
+	name := encodeDNSName(r.Name)
+	admn := encodeDNSName(r.Admin)
 
 	answer := &Answer{
 		Name:  0xC00C,
 		Type:  6,
 		Class: 1,
 		TTL:   0,
-		Data:  make([]byte, len(r.Name)+len(r.Admin)+20),
+		Data:  make([]byte, len(name)+len(admn)+20),
 	}
 
 	offset := 0
-	copy(answer.Data[offset:], r.Name)
-	offset += len(r.Name)
-	copy(answer.Data[offset:], r.Admin)
-	offset += len(r.Admin)
+	copy(answer.Data[offset:], name)
+	offset += len(name)
+	copy(answer.Data[offset:], admn)
+	offset += len(admn)
 	binary.BigEndian.PutUint32(answer.Data[offset:], r.Serial)
 	offset += 4
 	binary.BigEndian.PutUint32(answer.Data[offset:], r.Refresh)
