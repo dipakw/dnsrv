d00439c5dab4a41c331c8cf6e3948a5e0c4626af
Dipak Acharya <dev.dipak21@gmail.com>
2024-08-12 13:58:30 +0545

    Check region

--- dns/resolve.go
+++ dns/resolve.go
@@ -9,6 +9,7 @@ func resolve(ip net.IP, host string, rectype uint16) Entry {
 
 	name := "_@"
 	zone := zones[host]
+	regn := "US"
 
 	if zone == nil {
 		parts := strings.Split(host, ".")
@@ -36,48 +37,80 @@ func resolve(ip net.IP, host string, rectype uint16) Entry {
 
 		if rectype == 1 {
 			if entry, ok := zone.Records.A[name]; ok {
+				if regional, ok := entry.Regions[regn]; ok {
+					return regional
+				}
+
 				return entry.Default
 			}
 		}
 
 		if rectype == 28 {
 			if entry, ok := zone.Records.AAAA[name]; ok {
+				if regional, ok := entry.Regions[regn]; ok {
+					return regional
+				}
+
 				return entry.Default
 			}
 		}
 
 		if rectype == 16 {
 			if entry, ok := zone.Records.TXT[name]; ok {
+				if regional, ok := entry.Regions[regn]; ok {
+					return regional
+				}
+
 				return entry.Default
 			}
 		}
 
 		if rectype == 5 {
 			if entry, ok := zone.Records.CNAME[name]; ok {
+				if regional, ok := entry.Regions[regn]; ok {
+					return regional
+				}
+
 				return entry.Default
 			}
 		}
 
 		if rectype == 15 {
 			if entry, ok := zone.Records.MX[name]; ok {
+				if regional, ok := entry.Regions[regn]; ok {
+					return regional
+				}
+
 				return entry.Default
 			}
 		}
 
 		if rectype == 2 {
 			if entry, ok := zone.Records.NS[name]; ok {
+				if regional, ok := entry.Regions[regn]; ok {
+					return regional
+				}
+
 				return entry.Default
 			}
 		}
 
 		if rectype == 12 {
 			if entry, ok := zone.Records.PTR[name]; ok {
+				if regional, ok := entry.Regions[regn]; ok {
+					return regional
+				}
+
 				return entry.Default
 			}
 		}
 
 		if rectype == 33 {
 			if entry, ok := zone.Records.SRV[name]; ok {
+				if regional, ok := entry.Regions[regn]; ok {
+					return regional
+				}
+
 				return entry.Default
 			}
 		}
